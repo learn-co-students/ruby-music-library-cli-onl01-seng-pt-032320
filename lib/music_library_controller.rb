278b4be585c59f90e7ebca027b33011c61c45185
Abdul Khan <akhan17@outlook.com>
2020-04-26 01:38:33 +0000

    Done.

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -19,10 +19,20 @@ class MusicLibraryController
       puts "What would you like to do?"
 
     #user input
-      case input = gets.strip
+      input = gets.strip
 
-      when input = 'list artists'
-          list_artists
+      if input == 'list songs'
+        list_songs
+      elsif input == 'list artists'
+        list_artists
+      elsif input == 'list genres'
+        list_genres
+      elsif input == 'list artist'
+        list_songs_by_artist
+      elsif input == 'list genre'
+        list_songs_by_genre
+      elsif input == 'play song'
+        play_song
       end
 
     end
