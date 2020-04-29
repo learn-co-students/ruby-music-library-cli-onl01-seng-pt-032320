bfc615f1527b54dfdadec909f9e451615ae49ea2
Dina Kendle <dina.kendle@gmail.com>
2020-04-29 19:56:19 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,20 +1,44 @@
+require "pry"
 class Song 
-  attr_accessor :name, :artist
+  attr_accessor :name, :artist, :genre
   
   @@all = []
   
-  def initialize(name, artist = nil)
-    @name = name 
-    @artist = artist
+  def initialize(name, artist = nil, genre = nil)
+    @name = name
+    self.artist=(artist)
+    @genre = genre
     self.save
+   
+  end
+  ###
+  def artist=(artist)
+    if @artist == nil
+      @artist = artist
+    else
+      @artist = @artist
+    end
+    if self.artist != nil
+      @artist.add_song(self)
+    end
+    @artist
+  end
+  ###
+  
+  def genre=(genre)
+    @genre = genre
+    genre.songs << self
+  end
+
+  
+  
+  
+  def save 
+     @@all << self
   end
   
   def self.all
     @@all
-  end
-  
-  def save 
-     @@all << self
   end
   
   def self.destroy_all
