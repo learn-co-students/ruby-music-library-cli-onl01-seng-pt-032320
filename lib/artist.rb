458e21f563bc02a4e7bcd1c5a5f6a83b140abcaf
Dina Kendle <dina.kendle@gmail.com>
2020-04-29 17:12:51 +0000

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,11 +1,29 @@
 class Artist 
-  attr_accessor :name 
+  attr_accessor :name, :songs
   
   @@all = []
   
   def initialize(name)
     @name = name
+    @songs = []
     self.save
+  end
+  
+  def save
+    @@all << self
+  end
+  
+  def add_song(song)
+    if song.artist != self
+      song.artist = self
+    else 
+      song.artist
+    end
+    if !(@songs.include?(song))
+      @songs << song 
+    else 
+      @songs 
+    end
   end
   
   def self.all 
@@ -14,10 +32,6 @@ class Artist
   
   def self.destroy_all
     @@all.clear
-  end
-  
-  def save
-    @@all << self
   end
   
   def self.create(artist_obj)
