8ad605efa89f9961c1f53bc95a4864815eb52f23
Dina Kendle <dina.kendle@gmail.com>
2020-04-30 19:19:16 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,16 +1,16 @@
 require "pry"
-class Song 
+class Song
   attr_accessor :name
   attr_reader :artist, :genre
-  
+
   @@all = []
-  
+
   def initialize(name, artist = nil, genre = nil)
     @name = name
     self.artist=(artist)
     self.genre=(genre) if genre != nil
     self.save
-   
+
   end
 
 
@@ -25,36 +25,41 @@ class Song
     end
     @artist
   end
-  
-  
+
+
   def genre= (genre)
     @genre = genre
     genre.songs << self unless genre.songs.include?(self)
   end
-  
-  def save 
+
+  def save
      @@all << self
   end
-  
+
   def self.all
     @@all
   end
-  
+
   def self.destroy_all
     @@all.clear
   end
-  
-  def self.create(song_obj)
-    song_obj = Song.new(name)
+
+  def self.create(song_name)
+    song_obj = Song.new(song_name)
     @@all << song_obj
     song_obj
   end
-  
+
  def self.find_by_name(name)
-    @@all.select do |song_obj|
-      song_obj.name == name 
+    @@all.find do |song|
+        song.name == name
     end
   end
-  
-  
+
+  def self.find_or_create_by_name(song_name)
+      #binding.pry
+      self.find_by_name(song_name)
+  end
+
+
 end
