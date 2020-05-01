d6057838cf913db6e0953e9b72f51b9ed15ee953
Dina Kendle <dina.kendle@gmail.com>
2020-05-01 17:20:18 +0000

    Done.

--- lib/concerns/findable.rb
+++ lib/concerns/findable.rb
@@ -2,9 +2,10 @@ module Concerns
   module Findable
 
 
-      def find_by_name(string_name)
-        @@all.find do |klass_obj|
-          klass_obj.name == string_name
+      def find_by_name(name)
+    # @@all.detect{|a| a.name == name}
+        self.all.find do |obj|
+          obj.name == name
         end
       end
 
