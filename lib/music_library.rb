ea02b0bf2e3ed4b6e9e8747cc5ee3583547f70aa
aurel fontaine <ajfame11@gmail.com>
2020-05-02 06:55:24 +0000

    Done.

--- lib/music_library.rb
+++ lib/music_library.rb
@@ -28,4 +28,10 @@ class MusicLibraryController
       puts "#{idx}. #{song.artist.name} - #{song.name} - #{song.genre.name}"
     end
   end
+  
+  def list_artists
+    Artist.all.sort_by(&:name).each.with_index(1) do |art, idx|
+      puts "#{idx}. #{art.name}"
+    end
+  end
 end
