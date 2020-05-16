5ce0d8870a09ef6b369c6465e4b3839a1faa04c0
Bryce B <bryce.boddy@gmail.com>
2020-05-16 12:34:24 +0000

    Done.

--- lib/music_importer.rb
+++ lib/music_importer.rb
@@ -5,6 +5,10 @@ class MusicImporter
   end
   
   def files 
-    Dir.glob("#{path}/*").map{ |file| file.gsub("#{path}/",'')}
+    Dir.glob("#{path}/*").map { |file| file.gsub("#{path}/",'')}
+  end
+  
+  def import
+    files.each {|file| Song.create_from_filename(file)}
   end
 end
